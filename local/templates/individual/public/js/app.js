75f1abf33fa61d233bb9b0ff12eeb6077a2ddb6b
Никита <behappykit@mail.ru>
2019-03-19 10:26:45 +0500

    style(mobile-menu): fix safary scroll

--- local/templates/individual/public/js/app.js
+++ local/templates/individual/public/js/app.js
@@ -176,9 +176,6 @@ $(document).ready(function() {
             beforeOpen: function() {
                 $('.head-nav__modal').show().addClass('animated').addClass('slideInUp');
                 $('.js-init__menu').addClass('box-modal_close').addClass('fixed');
-                setTimeout(function() {
-                    $('.arcticmodal-container .animated').css('min-height', '100vh');
-                }, 1500);
             },
             beforeClose: function() {
                 $('.js-init__menu').removeClass('fixed').removeClass('box-modal_close');

89003c054df1274138facfbee160cf2917ae1782
Lukmanov Mikhail <lukmanof92@gmail.com>
2019-04-03 11:54:23 +0500

    fix section home

--- local/templates/individual/components/bitrix/catalog.section/home-shop_sidebar/lang/ru/.parameters.php
+++
@@ -1,8 +0,0 @@
-<?
-$MESS["NAME_SECTION_TITLE"] = "Заголовок блока";
-$MESS["NAME_SECTION_LINK"] = "Текст ссылки";
-$MESS["NAME_IMAGE_TYPE"] = "Тип вывода изображений";
-$MESS["NAME_IMAGE_TYPE_ALT"] = "Пропорционально";
-$MESS["NAME_IMAGE_TYPE_EXACT"] = "Растянуть";
-
-?>
--- local/templates/individual/components/bitrix/catalog.section/home-shop_sidebar/lang/ru/template.php
+++
@@ -1,6 +0,0 @@
-<?
-$MESS["CT_BCS_TPL_ELEMENT_DELETE_CONFIRM"] = "Будет удалена вся информация, связанная с этой записью. Продолжить?";
-
-$MESS["SECTION_TITLE_DEFAULT"] = "Популярные товары";
-$MESS["SECTION_LINK_DEFAULT"] = "Посмотреть все";
-?>

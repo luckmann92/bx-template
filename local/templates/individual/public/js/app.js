1926a21b150ec13cb1ba95c4b3614ccb4d3ceabc
Никита <behappykit@mail.ru>
2019-04-04 17:59:07 +0500

    fix(home-slider): fix autoheight

--- local/templates/individual/public/js/app.js
+++ local/templates/individual/public/js/app.js
@@ -145,7 +145,7 @@ $(document).ready(function() {
         ]
     });*/
 
-    $('.slider-home[data-autoheight = true]').on('setPosition', function () {
+    $('.slider-home[data-autoheight = false]').on('setPosition', function () {
         $(this).find('.slick-slide').height('auto');
         var slickTrack = $(this).find('.slick-track');
         var slickTrackHeight = $(slickTrack).height();
